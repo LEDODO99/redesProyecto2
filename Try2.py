3dafa4c02afadae8438b56b26b8ec168957fd21c
Luis <ledodo99@hotmail.com>
2020-09-23 18:57:31 -0600

    Agregar capacidad de agregar contacto y signup

--- Try2.py
+++ Try2.py
@@ -27,6 +27,88 @@ if sys.version_info < (3, 0):
 else:
     raw_input = input
 
+class RegisterBot(sleekxmpp.ClientXMPP):
+
+    """
+    A basic bot that will attempt to register an account
+    with an XMPP server.
+
+    NOTE: This follows the very basic registration workflow
+          from XEP-0077. More advanced server registration
+          workflows will need to check for data forms, etc.
+    """
+
+    def __init__(self, jid, password):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+
+        # The session_start event will be triggered when
+        # the bot establishes its connection with the server
+        # and the XML streams are ready for use. We want to
+        # listen for this event so that we we can initialize
+        # our roster.
+        self.add_event_handler("session_start", self.start, threaded=True)
+
+        # The register event provides an Iq result stanza with
+        # a registration form from the server. This may include
+        # the basic registration fields, a data form, an
+        # out-of-band URL, or any combination. For more advanced
+        # cases, you will need to examine the fields provided
+        # and respond accordingly. SleekXMPP provides plugins
+        # for data forms and OOB links that will make that easier.
+        self.add_event_handler("register", self.register, threaded=True)
+
+    def start(self, event):
+        """
+        Process the session_start event.
+
+        Typical actions for the session_start event are
+        requesting the roster and broadcasting an initial
+        presence stanza.
+
+        Arguments:
+            event -- An empty dictionary. The session_start
+                     event does not provide any additional
+                     data.
+        """
+        self.send_presence()
+        self.get_roster()
+
+        # We're only concerned about registering, so nothing more to do here.
+        self.disconnect()
+
+    def register(self, iq):
+        """
+        Fill out and submit a registration form.
+
+        The form may be composed of basic registration fields, a data form,
+        an out-of-band link, or any combination thereof. Data forms and OOB
+        links can be checked for as so:
+
+        if iq.match('iq/register/form'):
+            # do stuff with data form
+            # iq['register']['form']['fields']
+        if iq.match('iq/register/oob'):
+            # do stuff with OOB URL
+            # iq['register']['oob']['url']
+
+        To get the list of basic registration fields, you can use:
+            iq['register']['fields']
+        """
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            resp.send(now=True)
+            print("Se creo la cuenta para:",self.boundjid.user, ".\n En dominio:",self.boundjid.domain3)
+        except IqError as e:
+            logging.error("Could not register account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
 
 class RosterBrowser(sleekxmpp.ClientXMPP):
 
@@ -74,14 +156,17 @@ class RosterBrowser(sleekxmpp.ClientXMPP):
         
     def client_mannager(self):
         while (1):
-            a=input("Ingrese la opcion que desee:\n1. imprimir Roster.\n2. Enviar Mensaje directo\n3. Salir\n")
+            a=input("Ingrese la opcion que desee:\n1. imprimir Roster.\n2. Agregar un usuario a tu lista\n3. Enviar Mensaje directo\n4. Salir\n")
             if (a=="1"):
                 xmpp.print_contacts()
-            if(a=="2"):
+            if (a=="2"):
+                nombre=input("Ingrese el JID de el contacto a agregar")
+                self.send_presence_subscription(pto=nombre)
+            if(a=="3"):
                 recipient=input("Ingrese el usuario del receptor: ")
                 body=input("Ingrese el mensaje: ")
                 self.send_dm(recipient,body)
-            if (a=="3"):
+            if (a=="4"):
                 break
         self.disconnect()
 
@@ -145,41 +230,45 @@ class RosterBrowser(sleekxmpp.ClientXMPP):
                         print('       %s' % pres['status'])
 
 if __name__ == '__main__':
-    # Setup the command line arguments.
-    optp = OptionParser()
-    optp.add_option('-q','--quiet', help='set logging to ERROR',
-                    action='store_const',
-                    dest='loglevel',
-                    const=logging.ERROR,
-                    default=logging.ERROR)
-    optp.add_option('-d','--debug', help='set logging to DEBUG',
-                    action='store_const',
-                    dest='loglevel',
-                    const=logging.DEBUG,
-                    default=logging.ERROR)
-    optp.add_option('-v','--verbose', help='set logging to COMM',
-                    action='store_const',
-                    dest='loglevel',
-                    const=5,
-                    default=logging.ERROR)
+    while (1):
+        a=input("Seleccione lo que desea hacer\n1. Login\n2. Signup\n3. Salir\n")
+        if (a=="1"):
+            jid = raw_input("Username: ")
+            password = getpass.getpass("Password: ")
+            xmpp = RosterBrowser(jid, password)
+            if xmpp.connect():
+                # If you do not have the dnspython library installed, you will need
+                # to manually specify the name of the server if it does not match
+                # the one in the JID. For example, to use Google Talk you would
+                # need to use:
+                #
+                # if xmpp.connect(('talk.google.com', 5222)):
+                #     ...
+                xmpp.process(block=True)
+            else:
+                print("Unable to connect.")
+                exit()
+        if (a=="2"):
+            jid = raw_input("Username: ")
+            password = getpass.getpass("Password: ")
+            xmpp = RegisterBot(jid,password)
+            xmpp.register_plugin('xep_0030') # Service Discovery
+            xmpp.register_plugin('xep_0004') # Data forms
+            xmpp.register_plugin('xep_0066') # Out-of-band Data
+            xmpp.register_plugin('xep_0077') # In-band Registration
+            xmpp['xep_0077'].force_registration = True
+            if xmpp.connect():
+                # If you do not have the dnspython library installed, you will need
+                # to manually specify the name of the server if it does not match
+                # the one in the JID. For example, to use Google Talk you would
+                # need to use:
+                #
+                # if xmpp.connect(('talk.google.com', 5222)):
+                #     ...
+                xmpp.process(block=True)
+        if (a=="3"):
+            exit()
 
-    # JID and password options.
-    optp.add_option("-j", "--jid", dest="jid",
-                    help="JID to use")
-    optp.add_option("-p", "--password", dest="password",
-                    help="password to use")
-    opts,args = optp.parse_args()
-
-    # Setup logging.
-    logging.basicConfig(level=opts.loglevel,
-                        format='%(levelname)-8s %(message)s')
-
-    if opts.jid is None:
-        opts.jid = raw_input("Username: ")
-    if opts.password is None:
-        opts.password = getpass.getpass("Password: ")
-
-    xmpp = RosterBrowser(opts.jid, opts.password)
 
     # If you are working with an OpenFire server, you may need
     # to adjust the SSL version used:
@@ -189,17 +278,6 @@ if __name__ == '__main__':
     # xmpp.ca_certs = "path/to/ca/cert"
 
     # Connect to the XMPP server and start processing XMPP stanzas.
-    if xmpp.connect():
-        # If you do not have the dnspython library installed, you will need
-        # to manually specify the name of the server if it does not match
-        # the one in the JID. For example, to use Google Talk you would
-        # need to use:
-        #
-        # if xmpp.connect(('talk.google.com', 5222)):
-        #     ...
-        xmpp.process(block=True)
-    else:
-        print("Unable to connect.")
-        exit()
+    
     
 #als=Jabber("LuisDelgadoTry2","1234","redes2020.xyz")
